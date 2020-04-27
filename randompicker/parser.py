83cd2a0fc72e31cb5ff5ec0f75104999ffd93b13
m-vdb <me@maxvdb.com>
2020-04-27 21:33:57 +0200

    do not catch \@ or # in regex

--- randompicker/parser.py
+++ randompicker/parser.py
@@ -14,7 +14,7 @@ FREQUENCY_PATTERN = r'(on|every|next) (.+)'
 # /pickrandom @group to do something on Monday at 9am
 # /pickrandom @group to do something next Monday at 9am
 COMMAND_RE = re.compile(
-    fr'^<(?P<target>[@#][A-Z0-9]+)(?:|[^>]+)?>\s+'  # group or channel id
+    fr'^<[@#](?P<target>[A-Z0-9]+)(?:|[^>]+)?>\s+'  # group or channel id
     fr'to (?P<task>.+?)\s*'  # task to do
     fr'(?P<frequency>{FREQUENCY_PATTERN})?$'  # optional date or frequency
 )
